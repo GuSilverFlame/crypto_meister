6962dd8c44ba7ddb602e5f1ad061b2741d9e5a3b
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-02-06 14:03:45 -0200

    INITIAL COMMIT

---
+++ src/crypto_meister.cr
@@ -0,0 +1,6 @@
+require "./crypto_meister/*"
+
+# TODO: Write documentation for `CryptoMeister`
+module CryptoMeister
+  # TODO: Put your code here
+end
---
+++ src/crypto_meister/version.cr
@@ -0,0 +1,3 @@
+module CryptoMeister
+  VERSION = "0.0.1"
+end
