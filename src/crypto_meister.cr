0f8f679536c54fe9bdeb0fee1bd3b792251baf7f
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-02-06 15:49:33 -0200

    add methods for the central module to control everything

--- src/crypto_meister.cr
+++ src/crypto_meister.cr
@@ -1,6 +1,19 @@
 require "./crypto_meister/*"
 
-# TODO: Write documentation for `CryptoMeister`
 module CryptoMeister
-  # TODO: Put your code here
+  def self.new_agent(password : String, agent_type = :xor)
+    #TODO add more encryption agents and add to case
+    case agent_type
+    when :xor
+      XorEncryption.new(password)
+    else
+      raise "AgentTypeUnrecognized"
+    end
+  end
+
+  def self.process_text(text : String, encryption_agent = nil)
+    raise "EncryptionAgentMissing" unless encryption_agent
+    encryption_agent.process(text)
+  end
+
 end
