2f771f76e4a6348043e4cbf2b689a2d6f28e568b
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-02-07 16:15:00 -0200

    bugfix forgot to replace one password with key

--- src/crypto_meister.cr
+++ src/crypto_meister.cr
@@ -4,7 +4,7 @@ module CryptoMeister
   #TODO add more encryption agents
   AGENT_TYPES = {xor: XorCipher}
   def self.new_agent(key : String, agent_type = :xor)
-    AGENT_TYPES[agent_type].new(password)
+    AGENT_TYPES[agent_type].new(key)
   end
 
   def self.process_text(text : String, encryption_agent = nil)
