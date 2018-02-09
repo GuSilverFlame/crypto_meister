04b74b49f31e40dda7009ac6cf5533e224e4334a
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2018-02-09 12:00:56 -0200

    macro module to define block generators

---
+++ src/block_macros.cr
@@ -0,0 +1,16 @@
+module BlockMacros
+  macro define_block_generator(block_bytes, block_type)
+    private def generate_block_{{block_bytes}}(byte_array)
+      result_array = [] of {{block_type}}
+      byte_array.each_slice({{block_bytes}}) do |chunk_of_bytes|
+        block = {{block_type}}.new(0)
+        chunk_of_bytes.each do |byte|
+          block = block << 8
+          block += byte
+        end
+        result_array << block
+      end
+      result_array
+    end
+  end
+end
